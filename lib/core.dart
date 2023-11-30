bd39d2cba5ea0d027154cc6c00e183b3e455fc5e
RamsOkta <maddogdewo19@gmail.com>
2023-11-30 11:22:58 +0700

    flutter project

---
+++ lib/core.dart
@@ -0,0 +1,10 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:flutter_ui/module/dashboard/controller/dashboard_controller.dart';
+export 'package:flutter_ui/module/dashboard/view/dashboard_view.dart';
+export 'package:flutter_ui/module/login/controller/login_controller.dart';
+export 'package:flutter_ui/module/login/view/login_view.dart';
+export 'package:flutter_ui/state_util.dart';
---
+++ lib/module/dashboard/view/dashboard_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui/core.dart';
+import '../controller/dashboard_controller.dart';
+
+class DashboardView extends StatefulWidget {
+  const DashboardView({Key? key}) : super(key: key);
+
+  Widget build(context, DashboardController controller) {
+    controller.view = this;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: 100.0,
+                    color: Colors.red,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 100.0,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  State<DashboardView> createState() => DashboardController();
+}
